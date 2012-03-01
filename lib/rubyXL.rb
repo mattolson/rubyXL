a9313bb95e048dd9e948e9ea24d7fc532fc66bc5
Matt Olson <matt@frac.tl>
2012-02-29 16:58:18 -0800

    Fix include of new file

--- lib/rubyXL.rb
+++ lib/rubyXL.rb
@@ -1,6 +1,7 @@
 $: << File.expand_path(File.dirname(__FILE__))
 require File.expand_path(File.join(File.dirname(__FILE__),'rubyXL','workbook'))
 require File.expand_path(File.join(File.dirname(__FILE__),'rubyXL','private_class'))
+require File.expand_path(File.join(File.dirname(__FILE__),'rubyXL','sheet_data'))
 require File.expand_path(File.join(File.dirname(__FILE__),'rubyXL','worksheet'))
 require File.expand_path(File.join(File.dirname(__FILE__),'rubyXL','cell'))
 require File.expand_path(File.join(File.dirname(__FILE__),'rubyXL','parser'))

4b629bd0035f191cac081d70feec8b593fa6ede4
Matt Olson <matt@frac.tl>
2012-02-29 16:59:24 -0800

    Fix parse error

--- lib/rubyXL/sheet_data.rb
+++ lib/rubyXL/sheet_data.rb
@@ -1,18 +1,19 @@
 module RubyXL
-class SheetData
-  include Enumerable
+  class SheetData
+    include Enumerable
 
-  def initialize(values)
-    @data = values || Array.new
-  end
+    def initialize(values)
+      @data = values || Array.new
+    end
   
-  def [](x,y)
-    @data[x] ||= Array.new
-    @data[x][y]
-  end
+    def [](x,y)
+      @data[x] ||= Array.new
+      @data[x][y]
+    end
   
-  def []=(x,y,value)
-    @data[x] ||= Array.new
-    @data[x][y] = value
+    def []=(x,y,value)
+      @data[x] ||= Array.new
+      @data[x][y] = value
+    end
   end
 end
